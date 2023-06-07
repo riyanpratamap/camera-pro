5c415184149fae853787cd0c1c9dc7388c77eb69
Wisnu Riyan Pratama Putra <ext.wisnu.putra@grabtaxi.com>
2023-06-07 23:27:46 +0700

    Update react dependencies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Camera from 'react-camera-pro';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Camera App</h1>
+      <Camera
+        front={false}
+        width="100%"
+        height="auto"
+        onTakePhoto={dataUri => {
+          // Handle the captured photo data here
+          console.log(dataUri);
+        }}
+      />
     </div>
   );
 }
